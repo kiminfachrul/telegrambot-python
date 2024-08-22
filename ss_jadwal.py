b1172f1eae445334b1ac057897eb6aebd069356e
Muh. Fachrul Islam Sukimin <106242826+kiminfachrul@users.noreply.github.com>
2024-08-22 21:39:28 +0800

    Update ss_jadwal.py
    ganti semua file ke versi bisa /screenshot_now, untuk /schedule belum bisa

--- ss_jadwal.py
+++ ss_jadwal.py
@@ -1,50 +1,37 @@
+import schedule
+import time
+import pyautogui
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-import time
-import os
-from telegram import Bot
+from webdriver_manager.chrome import ChromeDriverManager
 
-# Path configuration
-CHROME_DRIVER_PATH = 'D:\chromedriver-win64\chromedriver.exe'  # Update this to your chromedriver path
-CHROME_EXECUTABLE_PATH = 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
-PROFILE_PATH = 'C:\\Users\\Fachrul Islam\\AppData\\Local\\Google\\Chrome\\User Data\\Default'
+import webbrowser
 
-# Initialize Chrome options
-chrome_options = Options()
-chrome_options.add_argument(f"user-data-dir={PROFILE_PATH}")
-chrome_options.binary_location = CHROME_EXECUTABLE_PATH
+user_data = "user-data-dir=C:\\Users\\Faul Islam\\AppData\\Local\\Google\\Chrome\\User Data\\Default" #Bisa di koment saja
+    
+# URL Spreadsheet yang ditargetkan
+SPREADSHEET_URL = "https://docs.google.com/spreadsheets/d/1e1wzZ1tSgQmsp7wEFon5QC-l9pu26s0PE7vkvEOMsiU/edit?hl=id&gid=909500613#gid=909500613"  # Ganti dengan URL spreadsheet yang diinginkan
 
-# Function to take a screenshot
-def take_screenshot(chat_id):
-    url = 'https://docs.google.com/spreadsheets/d/1e1wzZ1tSgQmsp7wEFon5QC-l9pu26s0PE7vkvEOMsiU/edit?gid=909500613#gid=909500613'  # Update this to your spreadsheet link
-    screenshot_path = f'screenshot_{int(time.time())}.png'
+def open_chrome_and_screenshot(save_path):
+    # Buka Chrome dan arahkan ke URL Spreadsheet
+    chrome_path = 'C:/Program Files/Google/Chrome/Application/chrome.exe %s' #Bisa cek di browser lalu cari chrome://version/
+      # Path ke aplikasi Chrome
+    webbrowser.get(chrome_path).open(SPREADSHEET_URL)
 
-    service = Service(CHROME_DRIVER_PATH)
-    driver = webdriver.Chrome(service=service, options=chrome_options)
+    # Beri waktu untuk Chrome memuat halaman
+    time.sleep(10)  # Sesuaikan waktu ini jika halaman membutuhkan waktu lebih lama untuk memuat
+    
+    # Mengambil screenshot seluruh layar
+    screenshot = pyautogui.screenshot()
+    screenshot.save(save_path)
+    print(f"Screenshot saved to {save_path}")
+    pyautogui.hotkey('alt', 'f4')
 
-    try:
-        driver.get(url)
-        time.sleep(5)  # Wait for the page to load
-
-        # Take screenshot
-        driver.save_screenshot(screenshot_path)
-
-        # Send the screenshot via Telegram
-        send_screenshot(chat_id, screenshot_path)
-    finally:
-        driver.quit()
-
-    # Clean up
-    if os.path.exists(screenshot_path):
-        os.remove(screenshot_path)
-
-def send_screenshot(chat_id, screenshot_path):
-    # Initialize bot
-    bot = Bot(token='7473429203:AAH_SA2JlEtIZZfQ9m7ysh0bLgAsduy1exo')
-
-    try:
-        with open(screenshot_path, 'rb') as photo:
-            bot.send_photo(chat_id=chat_id, photo=photo)
-    except Exception as e:
-        print(f"Error sending screenshot: {e}")
+def schedule_screenshot(save_path, schedule_time):
+    # Penjadwalan screenshot sesuai jadwal yang diinginkan
+    schedule.every().day.at(schedule_time).do(open_chrome_and_screenshot, save_path=save_path)
+    
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
